e482bdac7126848bdb5ae7a43cb43c23f0f6c070
Tony Plaza <tplaza3@gmail.com>
2014-09-08 11:19:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TonyBlog::Application.config.secret_token = '7f3ce5d77c60b20932d86045c8e761d1f8561a4b2501e1a45c6227fa8c85dbad761b4d922216a8dd2e617d7fb81b997e46a4265b3367ad06c1ae6259e1ebd9db'
